bc02466a47a1c4523aa4ed5da7387dc75a84ecfe
Gaurab <gaurabn23@gmail.com>
2025-01-20 16:45:54 +0545

    CheckAttendance added.

---
+++ If..Else Exercise/CheckAttendance.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class CheckAttendance {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int classHeld, classAttended;
+        float attendance;
+
+        System.out.println("Enter the number of classes held : ");
+        classHeld = input.nextInt();
+
+        System.out.println("Enter the number of classes attended : ");
+        classAttended = input.nextInt();
+
+        attendance = (float) ((classAttended / classHeld) * 100);
+
+        if (attendance < 75) {
+            System.out.println("You are not allowed to sit in the examination.");
+        } else {
+            System.out.println("You are allowed to sit in the examination");
+        }
+
+        input.close();
+    }
+}
